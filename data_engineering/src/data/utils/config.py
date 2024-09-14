0100b288443b29d393aeb2b88dc42c30b9490908
Adolfo Castro <adcasa@gmail.com>
2024-09-13 20:47:51 -0600

    Se agregar BookHotelRandamForest.ipynb para predecir cancelaciones por medio de randomforest

--- data_engineering/src/data/utils/config.py
+++ data_engineering/src/data/utils/config.py
@@ -1,6 +1,6 @@
 DATASETS = {
     'hotel_bookings': {
-        'url': 'https://raw.githubusercontent.com/azzubair01/Hotel_Booking_Analytics/master/hotel_bookings.csv',
+        'url': 'https://raw.githubusercontent.com/acastro/Hotel_Booking/master/hotel_bookings.csv',
         'destination': 'data/raw/hotel_bookings.csv'
     },
     'test_dataset': {
